4ca891db664c1e1cd724d32f4479a8ec36d4314d
kierarad <kieraradman@gmail.com>
2014-06-12 11:50:34 -0700

    Basic server

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import java.io.*;
+import java.net.*;
+
 class Main {
-	public static void main (String[] args) {
-		System.out.println("Hello!!");
+
+	public static void main (String[] args) throws java.lang.Exception {
+		System.out.println("Starting patu web server");
+		// s = new ServerSocket(8080);
+		// client = s.select
+		// output = client.openOutputStream
+		// output.write("Hello")
+		// client.close
+		final int port = 8080;
+		ServerSocket socket = new ServerSocket(port);
+		System.out.println("patu listening on " + port + "...");
+		Socket client = socket.accept();
+		System.out.println("Connection came in from: " + client.getRemoteSocketAddress());
+		OutputStream output = client.getOutputStream();
+		output.write("\nhello\n\n".getBytes());
+		System.out.println("done handling client.  shutting down");
 	}
 }
+
+
