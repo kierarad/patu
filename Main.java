dbf50e7865f9a2497bfffcc09510194f4ceee014
kierarad <kieraradman@gmail.com>
2014-06-12 12:19:02 -0700

    Server creates Threads for multiple clients and runs indefinitely

--- Main.java
+++ Main.java
@@ -1,23 +1,36 @@
 import java.io.*;
 import java.net.*;
+import java.lang.*;
 
 class Main {
 
 	public static void main (String[] args) throws java.lang.Exception {
 		System.out.println("Starting patu web server");
-		// s = new ServerSocket(8080);
-		// client = s.select
-		// output = client.openOutputStream
-		// output.write("Hello")
-		// client.close
 		final int port = 8080;
 		ServerSocket socket = new ServerSocket(port);
 		System.out.println("patu listening on " + port + "...");
-		Socket client = socket.accept();
-		System.out.println("Connection came in from: " + client.getRemoteSocketAddress());
-		OutputStream output = client.getOutputStream();
-		output.write("\nhello\n\n".getBytes());
-		System.out.println("done handling client.  shutting down");
+		while(true) {
+			System.out.println("Listening again for a client");
+	 		final Socket client = socket.accept();
+	 		new Thread(new Runnable() {
+	 			public void run() {
+	 					try {
+						System.out.println("Started new thread to handle connection: " + Thread.currentThread().getName());
+						System.out.println("zzzzz....");
+				 		Thread.sleep(10000);
+				 		System.out.println("I'm awake again");
+						System.out.println("Connection came in from: " + client.getRemoteSocketAddress());
+						OutputStream output = client.getOutputStream();
+						output.write("\nhello\n\n".getBytes());
+						System.out.println("done handling client");
+						client.close();
+					} catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+	 			}
+	 			
+	 		}).start();
+		}
 	}
 }
 
